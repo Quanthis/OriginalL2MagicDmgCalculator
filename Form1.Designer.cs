3b3fada8e60fca4c2536795464283b1b9bf74eec
Quanthis <56934469+Quanthis@users.noreply.github.com>
2023-07-18 20:29:32 +0200

    --Added basic design

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,7 +30,181 @@ namespace OriginalL2MagicDmgCalculator
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.CHARACTER_STATS_GROUP = new System.Windows.Forms.Panel();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.CHARACTER_STATS_LABEL = new System.Windows.Forms.Label();
+            this.LEVEL_LABEL = new System.Windows.Forms.Label();
+            this.INT_LABEL = new System.Windows.Forms.Label();
+            this.WIT_LABEL = new System.Windows.Forms.Label();
+            this.MEN_LABEL = new System.Windows.Forms.Label();
+            this.MATAK_LABEL = new System.Windows.Forms.Label();
+            this.CLASS_LABEL = new System.Windows.Forms.Label();
+            this.ATTRIBUTE_LABEL = new System.Windows.Forms.Label();
+            this.EQUIPMENT_GROUP = new System.Windows.Forms.Panel();
+            this.EQUIPMENT_LABEL = new System.Windows.Forms.Label();
+            this.BUFFS_GROUP = new System.Windows.Forms.Panel();
+            this.BUFFS_LABEL = new System.Windows.Forms.Label();
+            this.SKILLS_GROUP = new System.Windows.Forms.Panel();
+            this.SKILLS_LABEL = new System.Windows.Forms.Label();
+            this.CHARACTER_STATS_GROUP.SuspendLayout();
+            this.EQUIPMENT_GROUP.SuspendLayout();
+            this.BUFFS_GROUP.SuspendLayout();
+            this.SKILLS_GROUP.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // CHARACTER_STATS_GROUP
+            // 
+            this.CHARACTER_STATS_GROUP.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.CHARACTER_STATS_GROUP.Controls.Add(this.ATTRIBUTE_LABEL);
+            this.CHARACTER_STATS_GROUP.Controls.Add(this.CLASS_LABEL);
+            this.CHARACTER_STATS_GROUP.Controls.Add(this.MATAK_LABEL);
+            this.CHARACTER_STATS_GROUP.Controls.Add(this.MEN_LABEL);
+            this.CHARACTER_STATS_GROUP.Controls.Add(this.WIT_LABEL);
+            this.CHARACTER_STATS_GROUP.Controls.Add(this.INT_LABEL);
+            this.CHARACTER_STATS_GROUP.Controls.Add(this.LEVEL_LABEL);
+            this.CHARACTER_STATS_GROUP.Controls.Add(this.CHARACTER_STATS_LABEL);
+            this.CHARACTER_STATS_GROUP.Controls.Add(this.vScrollBar1);
+            this.CHARACTER_STATS_GROUP.ForeColor = System.Drawing.Color.White;
+            this.CHARACTER_STATS_GROUP.Location = new System.Drawing.Point(152, 430);
+            this.CHARACTER_STATS_GROUP.Name = "CHARACTER_STATS_GROUP";
+            this.CHARACTER_STATS_GROUP.Size = new System.Drawing.Size(408, 198);
+            this.CHARACTER_STATS_GROUP.TabIndex = 0;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(388, 0);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(20, 198);
+            this.vScrollBar1.TabIndex = 0;
+            // 
+            // CHARACTER_STATS_LABEL
+            // 
+            this.CHARACTER_STATS_LABEL.AutoSize = true;
+            this.CHARACTER_STATS_LABEL.Location = new System.Drawing.Point(3, 0);
+            this.CHARACTER_STATS_LABEL.Name = "CHARACTER_STATS_LABEL";
+            this.CHARACTER_STATS_LABEL.Size = new System.Drawing.Size(121, 20);
+            this.CHARACTER_STATS_LABEL.TabIndex = 1;
+            this.CHARACTER_STATS_LABEL.Text = "Character Stats";
+            // 
+            // LEVEL_LABEL
+            // 
+            this.LEVEL_LABEL.AutoSize = true;
+            this.LEVEL_LABEL.Location = new System.Drawing.Point(7, 24);
+            this.LEVEL_LABEL.Name = "LEVEL_LABEL";
+            this.LEVEL_LABEL.Size = new System.Drawing.Size(46, 20);
+            this.LEVEL_LABEL.TabIndex = 2;
+            this.LEVEL_LABEL.Text = "Level";
+            // 
+            // INT_LABEL
+            // 
+            this.INT_LABEL.AutoSize = true;
+            this.INT_LABEL.Location = new System.Drawing.Point(7, 56);
+            this.INT_LABEL.Name = "INT_LABEL";
+            this.INT_LABEL.Size = new System.Drawing.Size(34, 20);
+            this.INT_LABEL.TabIndex = 3;
+            this.INT_LABEL.Text = "INT";
+            // 
+            // WIT_LABEL
+            // 
+            this.WIT_LABEL.AutoSize = true;
+            this.WIT_LABEL.Location = new System.Drawing.Point(7, 90);
+            this.WIT_LABEL.Name = "WIT_LABEL";
+            this.WIT_LABEL.Size = new System.Drawing.Size(38, 20);
+            this.WIT_LABEL.TabIndex = 4;
+            this.WIT_LABEL.Text = "WIT";
+            // 
+            // MEN_LABEL
+            // 
+            this.MEN_LABEL.AutoSize = true;
+            this.MEN_LABEL.Location = new System.Drawing.Point(7, 121);
+            this.MEN_LABEL.Name = "MEN_LABEL";
+            this.MEN_LABEL.Size = new System.Drawing.Size(44, 20);
+            this.MEN_LABEL.TabIndex = 5;
+            this.MEN_LABEL.Text = "MEN";
+            // 
+            // MATAK_LABEL
+            // 
+            this.MATAK_LABEL.AutoSize = true;
+            this.MATAK_LABEL.Location = new System.Drawing.Point(191, 56);
+            this.MATAK_LABEL.Name = "MATAK_LABEL";
+            this.MATAK_LABEL.Size = new System.Drawing.Size(54, 20);
+            this.MATAK_LABEL.TabIndex = 6;
+            this.MATAK_LABEL.Text = "M. Atk";
+            // 
+            // CLASS_LABEL
+            // 
+            this.CLASS_LABEL.AutoSize = true;
+            this.CLASS_LABEL.Location = new System.Drawing.Point(191, 90);
+            this.CLASS_LABEL.Name = "CLASS_LABEL";
+            this.CLASS_LABEL.Size = new System.Drawing.Size(48, 20);
+            this.CLASS_LABEL.TabIndex = 7;
+            this.CLASS_LABEL.Text = "Class";
+            // 
+            // ATTRIBUTE_LABEL
+            // 
+            this.ATTRIBUTE_LABEL.AutoSize = true;
+            this.ATTRIBUTE_LABEL.Location = new System.Drawing.Point(191, 121);
+            this.ATTRIBUTE_LABEL.Name = "ATTRIBUTE_LABEL";
+            this.ATTRIBUTE_LABEL.Size = new System.Drawing.Size(70, 20);
+            this.ATTRIBUTE_LABEL.TabIndex = 8;
+            this.ATTRIBUTE_LABEL.Text = "Attribute";
+            // 
+            // EQUIPMENT_GROUP
+            // 
+            this.EQUIPMENT_GROUP.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.EQUIPMENT_GROUP.Controls.Add(this.EQUIPMENT_LABEL);
+            this.EQUIPMENT_GROUP.ForeColor = System.Drawing.Color.White;
+            this.EQUIPMENT_GROUP.Location = new System.Drawing.Point(703, 93);
+            this.EQUIPMENT_GROUP.Name = "EQUIPMENT_GROUP";
+            this.EQUIPMENT_GROUP.Size = new System.Drawing.Size(200, 100);
+            this.EQUIPMENT_GROUP.TabIndex = 1;
+            // 
+            // EQUIPMENT_LABEL
+            // 
+            this.EQUIPMENT_LABEL.AutoSize = true;
+            this.EQUIPMENT_LABEL.Location = new System.Drawing.Point(4, 0);
+            this.EQUIPMENT_LABEL.Name = "EQUIPMENT_LABEL";
+            this.EQUIPMENT_LABEL.Size = new System.Drawing.Size(86, 20);
+            this.EQUIPMENT_LABEL.TabIndex = 0;
+            this.EQUIPMENT_LABEL.Text = "Equipment";
+            // 
+            // BUFFS_GROUP
+            // 
+            this.BUFFS_GROUP.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BUFFS_GROUP.Controls.Add(this.BUFFS_LABEL);
+            this.BUFFS_GROUP.ForeColor = System.Drawing.Color.White;
+            this.BUFFS_GROUP.Location = new System.Drawing.Point(711, 280);
+            this.BUFFS_GROUP.Name = "BUFFS_GROUP";
+            this.BUFFS_GROUP.Size = new System.Drawing.Size(200, 100);
+            this.BUFFS_GROUP.TabIndex = 2;
+            // 
+            // BUFFS_LABEL
+            // 
+            this.BUFFS_LABEL.AutoSize = true;
+            this.BUFFS_LABEL.Location = new System.Drawing.Point(4, 0);
+            this.BUFFS_LABEL.Name = "BUFFS_LABEL";
+            this.BUFFS_LABEL.Size = new System.Drawing.Size(63, 20);
+            this.BUFFS_LABEL.TabIndex = 0;
+            this.BUFFS_LABEL.Text = "BUFFS";
+            // 
+            // SKILLS_GROUP
+            // 
+            this.SKILLS_GROUP.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.SKILLS_GROUP.Controls.Add(this.SKILLS_LABEL);
+            this.SKILLS_GROUP.ForeColor = System.Drawing.Color.White;
+            this.SKILLS_GROUP.Location = new System.Drawing.Point(347, 233);
+            this.SKILLS_GROUP.Name = "SKILLS_GROUP";
+            this.SKILLS_GROUP.Size = new System.Drawing.Size(200, 100);
+            this.SKILLS_GROUP.TabIndex = 3;
+            // 
+            // SKILLS_LABEL
+            // 
+            this.SKILLS_LABEL.AutoSize = true;
+            this.SKILLS_LABEL.Location = new System.Drawing.Point(4, 0);
+            this.SKILLS_LABEL.Name = "SKILLS_LABEL";
+            this.SKILLS_LABEL.Size = new System.Drawing.Size(64, 20);
+            this.SKILLS_LABEL.TabIndex = 0;
+            this.SKILLS_LABEL.Text = "SKILLS";
             // 
             // Form1
             // 
@@ -38,16 +212,45 @@ namespace OriginalL2MagicDmgCalculator
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.SKILLS_GROUP);
+            this.Controls.Add(this.BUFFS_GROUP);
+            this.Controls.Add(this.EQUIPMENT_GROUP);
+            this.Controls.Add(this.CHARACTER_STATS_GROUP);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "L2 Magic Dmg Calculator by Quan";
+            this.CHARACTER_STATS_GROUP.ResumeLayout(false);
+            this.CHARACTER_STATS_GROUP.PerformLayout();
+            this.EQUIPMENT_GROUP.ResumeLayout(false);
+            this.EQUIPMENT_GROUP.PerformLayout();
+            this.BUFFS_GROUP.ResumeLayout(false);
+            this.BUFFS_GROUP.PerformLayout();
+            this.SKILLS_GROUP.ResumeLayout(false);
+            this.SKILLS_GROUP.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel CHARACTER_STATS_GROUP;
+        private System.Windows.Forms.Label CHARACTER_STATS_LABEL;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.Label ATTRIBUTE_LABEL;
+        private System.Windows.Forms.Label CLASS_LABEL;
+        private System.Windows.Forms.Label MATAK_LABEL;
+        private System.Windows.Forms.Label MEN_LABEL;
+        private System.Windows.Forms.Label WIT_LABEL;
+        private System.Windows.Forms.Label INT_LABEL;
+        private System.Windows.Forms.Label LEVEL_LABEL;
+        private System.Windows.Forms.Panel EQUIPMENT_GROUP;
+        private System.Windows.Forms.Label EQUIPMENT_LABEL;
+        private System.Windows.Forms.Panel BUFFS_GROUP;
+        private System.Windows.Forms.Label BUFFS_LABEL;
+        private System.Windows.Forms.Panel SKILLS_GROUP;
+        private System.Windows.Forms.Label SKILLS_LABEL;
     }
 }
 
