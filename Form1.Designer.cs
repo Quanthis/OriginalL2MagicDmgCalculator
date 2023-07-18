1f8a3424a202c7c19e90ed2a9ffe21fc0dd8b6c4
Quanthis <56934469+Quanthis@users.noreply.github.com>
2023-07-18 20:39:22 +0200

    --Skills basic design

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,21 +31,27 @@ namespace OriginalL2MagicDmgCalculator
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.CHARACTER_STATS_GROUP = new System.Windows.Forms.Panel();
-            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
-            this.CHARACTER_STATS_LABEL = new System.Windows.Forms.Label();
-            this.LEVEL_LABEL = new System.Windows.Forms.Label();
-            this.INT_LABEL = new System.Windows.Forms.Label();
-            this.WIT_LABEL = new System.Windows.Forms.Label();
-            this.MEN_LABEL = new System.Windows.Forms.Label();
-            this.MATAK_LABEL = new System.Windows.Forms.Label();
-            this.CLASS_LABEL = new System.Windows.Forms.Label();
             this.ATTRIBUTE_LABEL = new System.Windows.Forms.Label();
+            this.CLASS_LABEL = new System.Windows.Forms.Label();
+            this.MATAK_LABEL = new System.Windows.Forms.Label();
+            this.MEN_LABEL = new System.Windows.Forms.Label();
+            this.WIT_LABEL = new System.Windows.Forms.Label();
+            this.INT_LABEL = new System.Windows.Forms.Label();
+            this.LEVEL_LABEL = new System.Windows.Forms.Label();
+            this.CHARACTER_STATS_LABEL = new System.Windows.Forms.Label();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
             this.EQUIPMENT_GROUP = new System.Windows.Forms.Panel();
             this.EQUIPMENT_LABEL = new System.Windows.Forms.Label();
             this.BUFFS_GROUP = new System.Windows.Forms.Panel();
             this.BUFFS_LABEL = new System.Windows.Forms.Label();
             this.SKILLS_GROUP = new System.Windows.Forms.Panel();
             this.SKILLS_LABEL = new System.Windows.Forms.Label();
+            this.NUKE_TYPE_LABEL = new System.Windows.Forms.Label();
+            this.NUKE_LEVEL_LABEL = new System.Windows.Forms.Label();
+            this.NUKE_POWER_LABEL = new System.Windows.Forms.Label();
+            this.NUKE_ENCHANT_TYPE_LABEL = new System.Windows.Forms.Label();
+            this.NUKE_ENCHANT_POWER_LABEL = new System.Windows.Forms.Label();
+            this.NUKE_ELE_ALIGN = new System.Windows.Forms.Label();
             this.CHARACTER_STATS_GROUP.SuspendLayout();
             this.EQUIPMENT_GROUP.SuspendLayout();
             this.BUFFS_GROUP.SuspendLayout();
@@ -70,12 +76,68 @@ namespace OriginalL2MagicDmgCalculator
             this.CHARACTER_STATS_GROUP.Size = new System.Drawing.Size(408, 198);
             this.CHARACTER_STATS_GROUP.TabIndex = 0;
             // 
-            // vScrollBar1
+            // ATTRIBUTE_LABEL
             // 
-            this.vScrollBar1.Location = new System.Drawing.Point(388, 0);
-            this.vScrollBar1.Name = "vScrollBar1";
-            this.vScrollBar1.Size = new System.Drawing.Size(20, 198);
-            this.vScrollBar1.TabIndex = 0;
+            this.ATTRIBUTE_LABEL.AutoSize = true;
+            this.ATTRIBUTE_LABEL.Location = new System.Drawing.Point(191, 160);
+            this.ATTRIBUTE_LABEL.Name = "ATTRIBUTE_LABEL";
+            this.ATTRIBUTE_LABEL.Size = new System.Drawing.Size(70, 20);
+            this.ATTRIBUTE_LABEL.TabIndex = 8;
+            this.ATTRIBUTE_LABEL.Text = "Attribute";
+            // 
+            // CLASS_LABEL
+            // 
+            this.CLASS_LABEL.AutoSize = true;
+            this.CLASS_LABEL.Location = new System.Drawing.Point(191, 120);
+            this.CLASS_LABEL.Name = "CLASS_LABEL";
+            this.CLASS_LABEL.Size = new System.Drawing.Size(48, 20);
+            this.CLASS_LABEL.TabIndex = 7;
+            this.CLASS_LABEL.Text = "Class";
+            // 
+            // MATAK_LABEL
+            // 
+            this.MATAK_LABEL.AutoSize = true;
+            this.MATAK_LABEL.Location = new System.Drawing.Point(191, 80);
+            this.MATAK_LABEL.Name = "MATAK_LABEL";
+            this.MATAK_LABEL.Size = new System.Drawing.Size(54, 20);
+            this.MATAK_LABEL.TabIndex = 6;
+            this.MATAK_LABEL.Text = "M. Atk";
+            // 
+            // MEN_LABEL
+            // 
+            this.MEN_LABEL.AutoSize = true;
+            this.MEN_LABEL.Location = new System.Drawing.Point(11, 160);
+            this.MEN_LABEL.Name = "MEN_LABEL";
+            this.MEN_LABEL.Size = new System.Drawing.Size(44, 20);
+            this.MEN_LABEL.TabIndex = 5;
+            this.MEN_LABEL.Text = "MEN";
+            // 
+            // WIT_LABEL
+            // 
+            this.WIT_LABEL.AutoSize = true;
+            this.WIT_LABEL.Location = new System.Drawing.Point(11, 120);
+            this.WIT_LABEL.Name = "WIT_LABEL";
+            this.WIT_LABEL.Size = new System.Drawing.Size(38, 20);
+            this.WIT_LABEL.TabIndex = 4;
+            this.WIT_LABEL.Text = "WIT";
+            // 
+            // INT_LABEL
+            // 
+            this.INT_LABEL.AutoSize = true;
+            this.INT_LABEL.Location = new System.Drawing.Point(11, 80);
+            this.INT_LABEL.Name = "INT_LABEL";
+            this.INT_LABEL.Size = new System.Drawing.Size(34, 20);
+            this.INT_LABEL.TabIndex = 3;
+            this.INT_LABEL.Text = "INT";
+            // 
+            // LEVEL_LABEL
+            // 
+            this.LEVEL_LABEL.AutoSize = true;
+            this.LEVEL_LABEL.Location = new System.Drawing.Point(7, 40);
+            this.LEVEL_LABEL.Name = "LEVEL_LABEL";
+            this.LEVEL_LABEL.Size = new System.Drawing.Size(46, 20);
+            this.LEVEL_LABEL.TabIndex = 2;
+            this.LEVEL_LABEL.Text = "Level";
             // 
             // CHARACTER_STATS_LABEL
             // 
@@ -86,77 +148,21 @@ namespace OriginalL2MagicDmgCalculator
             this.CHARACTER_STATS_LABEL.TabIndex = 1;
             this.CHARACTER_STATS_LABEL.Text = "Character Stats";
             // 
-            // LEVEL_LABEL
+            // vScrollBar1
             // 
-            this.LEVEL_LABEL.AutoSize = true;
-            this.LEVEL_LABEL.Location = new System.Drawing.Point(7, 24);
-            this.LEVEL_LABEL.Name = "LEVEL_LABEL";
-            this.LEVEL_LABEL.Size = new System.Drawing.Size(46, 20);
-            this.LEVEL_LABEL.TabIndex = 2;
-            this.LEVEL_LABEL.Text = "Level";
-            // 
-            // INT_LABEL
-            // 
-            this.INT_LABEL.AutoSize = true;
-            this.INT_LABEL.Location = new System.Drawing.Point(7, 56);
-            this.INT_LABEL.Name = "INT_LABEL";
-            this.INT_LABEL.Size = new System.Drawing.Size(34, 20);
-            this.INT_LABEL.TabIndex = 3;
-            this.INT_LABEL.Text = "INT";
-            // 
-            // WIT_LABEL
-            // 
-            this.WIT_LABEL.AutoSize = true;
-            this.WIT_LABEL.Location = new System.Drawing.Point(7, 90);
-            this.WIT_LABEL.Name = "WIT_LABEL";
-            this.WIT_LABEL.Size = new System.Drawing.Size(38, 20);
-            this.WIT_LABEL.TabIndex = 4;
-            this.WIT_LABEL.Text = "WIT";
-            // 
-            // MEN_LABEL
-            // 
-            this.MEN_LABEL.AutoSize = true;
-            this.MEN_LABEL.Location = new System.Drawing.Point(7, 121);
-            this.MEN_LABEL.Name = "MEN_LABEL";
-            this.MEN_LABEL.Size = new System.Drawing.Size(44, 20);
-            this.MEN_LABEL.TabIndex = 5;
-            this.MEN_LABEL.Text = "MEN";
-            // 
-            // MATAK_LABEL
-            // 
-            this.MATAK_LABEL.AutoSize = true;
-            this.MATAK_LABEL.Location = new System.Drawing.Point(191, 56);
-            this.MATAK_LABEL.Name = "MATAK_LABEL";
-            this.MATAK_LABEL.Size = new System.Drawing.Size(54, 20);
-            this.MATAK_LABEL.TabIndex = 6;
-            this.MATAK_LABEL.Text = "M. Atk";
-            // 
-            // CLASS_LABEL
-            // 
-            this.CLASS_LABEL.AutoSize = true;
-            this.CLASS_LABEL.Location = new System.Drawing.Point(191, 90);
-            this.CLASS_LABEL.Name = "CLASS_LABEL";
-            this.CLASS_LABEL.Size = new System.Drawing.Size(48, 20);
-            this.CLASS_LABEL.TabIndex = 7;
-            this.CLASS_LABEL.Text = "Class";
-            // 
-            // ATTRIBUTE_LABEL
-            // 
-            this.ATTRIBUTE_LABEL.AutoSize = true;
-            this.ATTRIBUTE_LABEL.Location = new System.Drawing.Point(191, 121);
-            this.ATTRIBUTE_LABEL.Name = "ATTRIBUTE_LABEL";
-            this.ATTRIBUTE_LABEL.Size = new System.Drawing.Size(70, 20);
-            this.ATTRIBUTE_LABEL.TabIndex = 8;
-            this.ATTRIBUTE_LABEL.Text = "Attribute";
+            this.vScrollBar1.Location = new System.Drawing.Point(388, 0);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(20, 198);
+            this.vScrollBar1.TabIndex = 0;
             // 
             // EQUIPMENT_GROUP
             // 
             this.EQUIPMENT_GROUP.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.EQUIPMENT_GROUP.Controls.Add(this.EQUIPMENT_LABEL);
             this.EQUIPMENT_GROUP.ForeColor = System.Drawing.Color.White;
-            this.EQUIPMENT_GROUP.Location = new System.Drawing.Point(703, 93);
+            this.EQUIPMENT_GROUP.Location = new System.Drawing.Point(163, 86);
             this.EQUIPMENT_GROUP.Name = "EQUIPMENT_GROUP";
-            this.EQUIPMENT_GROUP.Size = new System.Drawing.Size(200, 100);
+            this.EQUIPMENT_GROUP.Size = new System.Drawing.Size(397, 239);
             this.EQUIPMENT_GROUP.TabIndex = 1;
             // 
             // EQUIPMENT_LABEL
@@ -173,9 +179,9 @@ namespace OriginalL2MagicDmgCalculator
             this.BUFFS_GROUP.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.BUFFS_GROUP.Controls.Add(this.BUFFS_LABEL);
             this.BUFFS_GROUP.ForeColor = System.Drawing.Color.White;
-            this.BUFFS_GROUP.Location = new System.Drawing.Point(711, 280);
+            this.BUFFS_GROUP.Location = new System.Drawing.Point(719, 86);
             this.BUFFS_GROUP.Name = "BUFFS_GROUP";
-            this.BUFFS_GROUP.Size = new System.Drawing.Size(200, 100);
+            this.BUFFS_GROUP.Size = new System.Drawing.Size(390, 239);
             this.BUFFS_GROUP.TabIndex = 2;
             // 
             // BUFFS_LABEL
@@ -190,11 +196,17 @@ namespace OriginalL2MagicDmgCalculator
             // SKILLS_GROUP
             // 
             this.SKILLS_GROUP.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.SKILLS_GROUP.Controls.Add(this.NUKE_ELE_ALIGN);
+            this.SKILLS_GROUP.Controls.Add(this.NUKE_ENCHANT_POWER_LABEL);
+            this.SKILLS_GROUP.Controls.Add(this.NUKE_ENCHANT_TYPE_LABEL);
+            this.SKILLS_GROUP.Controls.Add(this.NUKE_POWER_LABEL);
+            this.SKILLS_GROUP.Controls.Add(this.NUKE_LEVEL_LABEL);
+            this.SKILLS_GROUP.Controls.Add(this.NUKE_TYPE_LABEL);
             this.SKILLS_GROUP.Controls.Add(this.SKILLS_LABEL);
             this.SKILLS_GROUP.ForeColor = System.Drawing.Color.White;
-            this.SKILLS_GROUP.Location = new System.Drawing.Point(347, 233);
+            this.SKILLS_GROUP.Location = new System.Drawing.Point(711, 430);
             this.SKILLS_GROUP.Name = "SKILLS_GROUP";
-            this.SKILLS_GROUP.Size = new System.Drawing.Size(200, 100);
+            this.SKILLS_GROUP.Size = new System.Drawing.Size(398, 198);
             this.SKILLS_GROUP.TabIndex = 3;
             // 
             // SKILLS_LABEL
@@ -205,6 +217,60 @@ namespace OriginalL2MagicDmgCalculator
             this.SKILLS_LABEL.Size = new System.Drawing.Size(64, 20);
             this.SKILLS_LABEL.TabIndex = 0;
             this.SKILLS_LABEL.Text = "SKILLS";
+            // 
+            // NUKE_TYPE_LABEL
+            // 
+            this.NUKE_TYPE_LABEL.AutoSize = true;
+            this.NUKE_TYPE_LABEL.Location = new System.Drawing.Point(12, 40);
+            this.NUKE_TYPE_LABEL.Name = "NUKE_TYPE_LABEL";
+            this.NUKE_TYPE_LABEL.Size = new System.Drawing.Size(84, 20);
+            this.NUKE_TYPE_LABEL.TabIndex = 1;
+            this.NUKE_TYPE_LABEL.Text = "Nuke Type";
+            // 
+            // NUKE_LEVEL_LABEL
+            // 
+            this.NUKE_LEVEL_LABEL.AutoSize = true;
+            this.NUKE_LEVEL_LABEL.Location = new System.Drawing.Point(12, 80);
+            this.NUKE_LEVEL_LABEL.Name = "NUKE_LEVEL_LABEL";
+            this.NUKE_LEVEL_LABEL.Size = new System.Drawing.Size(87, 20);
+            this.NUKE_LEVEL_LABEL.TabIndex = 2;
+            this.NUKE_LEVEL_LABEL.Text = "Nuke Level";
+            // 
+            // NUKE_POWER_LABEL
+            // 
+            this.NUKE_POWER_LABEL.AutoSize = true;
+            this.NUKE_POWER_LABEL.Location = new System.Drawing.Point(12, 120);
+            this.NUKE_POWER_LABEL.Name = "NUKE_POWER_LABEL";
+            this.NUKE_POWER_LABEL.Size = new System.Drawing.Size(94, 20);
+            this.NUKE_POWER_LABEL.TabIndex = 3;
+            this.NUKE_POWER_LABEL.Text = "Nuke Power";
+            // 
+            // NUKE_ENCHANT_TYPE_LABEL
+            // 
+            this.NUKE_ENCHANT_TYPE_LABEL.AutoSize = true;
+            this.NUKE_ENCHANT_TYPE_LABEL.Location = new System.Drawing.Point(175, 80);
+            this.NUKE_ENCHANT_TYPE_LABEL.Name = "NUKE_ENCHANT_TYPE_LABEL";
+            this.NUKE_ENCHANT_TYPE_LABEL.Size = new System.Drawing.Size(148, 20);
+            this.NUKE_ENCHANT_TYPE_LABEL.TabIndex = 4;
+            this.NUKE_ENCHANT_TYPE_LABEL.Text = "Nuke Enchant Type";
+            // 
+            // NUKE_ENCHANT_POWER_LABEL
+            // 
+            this.NUKE_ENCHANT_POWER_LABEL.AutoSize = true;
+            this.NUKE_ENCHANT_POWER_LABEL.Location = new System.Drawing.Point(175, 120);
+            this.NUKE_ENCHANT_POWER_LABEL.Name = "NUKE_ENCHANT_POWER_LABEL";
+            this.NUKE_ENCHANT_POWER_LABEL.Size = new System.Drawing.Size(158, 20);
+            this.NUKE_ENCHANT_POWER_LABEL.TabIndex = 5;
+            this.NUKE_ENCHANT_POWER_LABEL.Text = "Nuke Enchant Power";
+            // 
+            // NUKE_ELE_ALIGN
+            // 
+            this.NUKE_ELE_ALIGN.AutoSize = true;
+            this.NUKE_ELE_ALIGN.Location = new System.Drawing.Point(12, 160);
+            this.NUKE_ELE_ALIGN.Name = "NUKE_ELE_ALIGN";
+            this.NUKE_ELE_ALIGN.Size = new System.Drawing.Size(238, 20);
+            this.NUKE_ELE_ALIGN.TabIndex = 6;
+            this.NUKE_ELE_ALIGN.Text = "Nuke same elemens as weapon:";
             // 
             // Form1
             // 
@@ -251,6 +317,12 @@ namespace OriginalL2MagicDmgCalculator
         private System.Windows.Forms.Label BUFFS_LABEL;
         private System.Windows.Forms.Panel SKILLS_GROUP;
         private System.Windows.Forms.Label SKILLS_LABEL;
+        private System.Windows.Forms.Label NUKE_ELE_ALIGN;
+        private System.Windows.Forms.Label NUKE_ENCHANT_POWER_LABEL;
+        private System.Windows.Forms.Label NUKE_ENCHANT_TYPE_LABEL;
+        private System.Windows.Forms.Label NUKE_POWER_LABEL;
+        private System.Windows.Forms.Label NUKE_LEVEL_LABEL;
+        private System.Windows.Forms.Label NUKE_TYPE_LABEL;
     }
 }
 
